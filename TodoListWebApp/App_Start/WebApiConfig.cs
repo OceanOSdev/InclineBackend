7ef0a937dbfa3685e37f4a93c4d49777b3c18109
Thomas Maloney <thomastnf@live.com>
2016-05-08 18:58:22 -0500

    Added Web Api

---
+++ TodoListWebApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace TodoListWebApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //Web Api routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+
+            // WebAPI when dealing with JSON & JavaScript
+            // Setup json serialization to serialize classes to camel (std. Json format)
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            formatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
--- TodoListWebApp/Global.asax.cs
+++ TodoListWebApp/Global.asax.cs
@@ -5,6 +5,7 @@ using System.IdentityModel.Claims;
 using System.Linq;
 using System.Web;
 using System.Web.Helpers;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -17,7 +18,13 @@ namespace TodoListWebApp
         protected void Application_Start()
         {
             Database.SetInitializer<TodoListWebAppContext>(new TodoListWebAppInitializer());
+            // Default Stuff
             AreaRegistration.RegisterAllAreas();
+
+            // Manually installed web api 2 after making an mvc project
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // More Default Stuff
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
