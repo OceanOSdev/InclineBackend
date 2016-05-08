10cbe13c3994efbeda3e8af399f21b36cbee7c09
Thomas Maloney <thomastnf@live.com>
2016-05-08 18:40:44 -0500

    Added Drop down for nav bar and cleaned up some html

---
+++ TodoListWebApp/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using TodoListWebApp.DAL;
+
+namespace TodoListWebApp.Controllers
+{
+    [Authorize]
+    public class HealthController : Controller
+    {
+        private TodoListWebAppContext db = new TodoListWebAppContext();
+
+        // GET: Health
+        public ActionResult Index()
+        {
+            string owner = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return View();
+        }
+    }
+}
